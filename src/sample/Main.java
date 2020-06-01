8d0cab27dac3deed7d5b80c404289cec5c8e486e
Satoru-cmd/ <satoruxiaoze@gmail.com>
2020-06-01 16:46:38 +0900

    6/1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,36 @@
 package sample;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("aaa");
-		System.out.println("Hello World");
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.println("３身の整数の最大値を求めます");
+		int a = stdIn.nextInt();
+		int b = stdIn.nextInt();
+		int c = stdIn.nextInt();
+
+		System.out.println(med3(a, b, c));
+	}
+
+	static int med3(int a, int b, int c) {
+		if(a >= b) {
+			if(b >= c) {
+				return b;
+			}else if(a <= c) {
+				return a;
+			}else {
+				return c;
+			}
+		}else if(a > c) {
+			return a;
+		}else if(b > c) {
+			return c;
+		}else {
+			return b;
+		}
 	}
 
 }
