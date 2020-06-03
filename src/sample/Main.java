a0399184cf1bd0884fe8191c4491cc2e812e52de
Satoru-cmd/ <satoruxiaoze@gmail.com>
2020-06-03 16:14:09 +0900

    6/3

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,31 +6,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		System.out.println("３身の整数の最大値を求めます");
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-
-		System.out.println(med3(a, b, c));
-	}
-
-	static int med3(int a, int b, int c) {
-		if(a >= b) {
-			if(b >= c) {
-				return b;
-			}else if(a <= c) {
-				return a;
-			}else {
-				return c;
-			}
-		}else if(a > c) {
-			return a;
-		}else if(b > c) {
-			return c;
-		}else {
-			return b;
+		int n = sc.nextInt();
+		int sum = 0;
+		for(int i = 1; i <= n; i++) {
+			System.out.print(i + "+");
+			sum += i;
 		}
+		System.out.println("=" +sum);
+
 	}
+
 
 }
+
+
