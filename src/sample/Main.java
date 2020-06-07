bd7c605b573dd6d0a4ea28cd7cb9be60067e9688
Satoru-cmd/ <satoruxiaoze@gmail.com>
2020-06-07 18:20:34 +0900

    primenumber(1)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,11 +4,10 @@ import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) {
-		int[] a = {1, 2, 4, 5, 6, 7, 8};
-		rcopy(a);
-
 
 	}
+
+	//配列aを逆順にしてコピー
 	static void rcopy(int[] a) {
 		int n = a.length;
 		int b[] = new int[n];
@@ -18,5 +17,18 @@ public class Main {
 			m--;
 		}
 		System.out.println(Arrays.toString(b));
+	}
+
+	//整数aの素数判定
+	static void primenumber(int a) {
+		for(int i = 2; i < a; i++) {
+			if(a % i == 0) {
+				System.out.println("素数ではない");
+				break;
+			}
+			if(a == i+1) {
+				System.out.println("素数");
+			}
+		}
 	}
 }
